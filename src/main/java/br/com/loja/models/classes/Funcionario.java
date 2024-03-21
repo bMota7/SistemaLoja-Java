c6285ed0cc4da7dce8bd1d7f6f07d08b0568437b
Bruno Mota <117960184+bMota7@users.noreply.github.com>
2024-03-21 16:55:00 -0300

    Update Funcionario.java

--- src/main/java/br/com/loja/models/classes/Funcionario.java
+++ src/main/java/br/com/loja/models/classes/Funcionario.java
@@ -1,2 +1,52 @@
-package br.com.loja.models.classes;public class Funcionario {
+package br.com.loja.models.classes;
+
+import java.util.Scanner;
+
+public class Funcionario implements Cadastra, ImprimeDados{
+    private String nomeFuncionario;
+    private String funcao;
+
+    public Funcionario(String nomeFuncionario, String funcao) {
+        this.nomeFuncionario = nomeFuncionario;
+        this.funcao = funcao;
+    }
+
+    public String getNomeFuncionario() {
+        return nomeFuncionario;
+    }
+
+    public void setNomeFuncionario(String nomeFuncionario) {
+        this.nomeFuncionario = nomeFuncionario;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    @Override
+    public void cadastra() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("*** *** *** ***");
+        System.out.println("Cadastro Funcionario\n");
+        System.out.println("Nome funcionario: ");
+        nomeFuncionario = sc.nextLine();
+        System.out.println("Funcao: ");
+        funcao = sc.nextLine();
+        System.out.println("*** *** *** ***\n");
+
+        System.out.println("CADASTRO FEITO COM SUCESSO!");
+    }
+
+    @Override
+    public void imprimeDados() {
+        System.out.println("\n=== === === ===");
+        System.out.println("Cadastro Funcionario: ");
+        System.out.println("Nome do funcionario: " + nomeFuncionario);
+        System.out.println("Funcao: " + funcao);
+        System.out.println("=== === === ===");
+    }
 }
